0c3a42c99d3f51b5359f7acb3f1756c07b7a39ed
Kevin Joyeux <joyeuxkevin@orange.fr>
2023-06-20 15:52:21 +0200

    20/06

---
+++ demo/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Mettre à jour le code source
+git pull
+# Construire le projet
+bash mvnw package -P prod -e
+# Construire l'image Docker
+docker build --no-cache -t image-spring-kevin
+# Arreter le conteneur existant
+docker stop conteneur-spring-kevin
+# Supprimer le conteneur existant
+docker rm -f conteneur-spring-kevin
+# Lancer un nouveau conteneur
+docker run -d --net backend --ip 172.18.0.4 --name=conteneur-spring-kevin -p 8080:8080 -v uploaded_files:/uploads image-spring-kevin
