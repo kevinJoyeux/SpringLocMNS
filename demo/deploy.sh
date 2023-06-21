472418d4837a17722c3e5bfdf748a048b30d4486
Kevin Joyeux <joyeuxkevin@orange.fr>
2023-06-21 09:27:33 +0200

    LocMns 21/06

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -10,4 +10,4 @@ docker stop conteneur-spring-kevin
 # Supprimer le conteneur existant
 docker rm -f conteneur-spring-kevin
 # Lancer un nouveau conteneur
-docker run -d --net backend --ip 172.18.0.4 --name=conteneur-spring-kevin -p 8080:8080 -v uploaded_files:/uploads image-spring-kevin
+docker run -d --net backend --ip 172.18.0.4 --name=conteneur-spring-kevin -p 8181:8080 -v uploaded_files:/uploads image-spring-kevin
