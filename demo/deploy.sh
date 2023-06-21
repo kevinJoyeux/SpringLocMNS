8773b236206cbcda549079027b7d788137bc7687
Kevin Joyeux <joyeuxkevin@orange.fr>
2023-06-21 08:51:32 +0200

    LocMns 21/06

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -2,7 +2,7 @@
 # Mettre à jour le code source
 git pull
 # Construire le projet
-bash mvnw package -P prod, sysadmin --settings /home/debian/.m2/settings.xml
+bash mvnw package -P prod,sysadmin --settings /home/debian/.m2/settings.xml
 # Construire l'image Docker
 docker build --no-cache -t image-spring-kevin.
 # Arreter le conteneur existant
